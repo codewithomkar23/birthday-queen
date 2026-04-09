911ab7681980fbf6188b7e3a6555b55a9f86c0b1
codewithomkar23 <ladomii53@gmail.com>
2026-04-09 17:37:15 +0530

    Update love.js

--- love.js
+++ love.js
@@ -123,7 +123,7 @@
         },
         clear: function () {
             var ctx = this.tree.ctx, cirle = this.cirle;
-            var point = cirle.point, scale = cirle.scale, radius = 26;
+            var point = cirle.point, scale = cirle.scale, radius = 50;
             var w = h = (radius * scale);
             ctx.clearRect(point.x - w, point.y - h, 4 * w, 4 * h);
         },
